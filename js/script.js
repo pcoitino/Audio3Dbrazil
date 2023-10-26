88fd5c3270fb61bec22ba2e22cca48dbd0b9297c
pcoitino <149019053+pcoitino@users.noreply.github.com>
2023-10-26 15:35:59 -0300

    Update2 script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-const RADIO_NAME = 'Binaulab Audio 3D';
+const RADIO_NAME = 'Binaulab_Audio_3D';
 
 // SELECT ARTWORK PROVIDER, ITUNES, DEEZER & SPOTIFY  eg : spotify 
 var API_SERVICE = 'SPOTIFY';
